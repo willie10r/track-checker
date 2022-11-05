d1d6966fa11babaeab248277cda8f6187a78c06e
willie m <zx10rwillie@gmail.com>
2022-11-04 20:23:49 -0500

    finally got the api working

--- src/components/home.js
+++ src/components/home.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    async function getWeather(part) {
-        const response = await fetch('http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json')
-        const data = await response.json();
-        return (data.dataseries[2].seeing);
-    }
-    let show = getWeather();
+    useEffect(() => {
+        fetch('http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json')
+            //this is for checking for https response issues the if else statment
+            .then((response) => {
+                if (!response.ok) {
+                    console.log('http error')
+                }
+                return response.json()
+            })
+            .then((data) =>
+                setData(data)
+            )
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }, []);
+    let show = data.dataseries[2].cloudcover;
 
     return (
         <div>
             <h1>track weather</h1>
             <section>
                 <h2>local weather</h2>
-                <h4>{`the rain is ${show}`}</h4>
+                <h4>{`${show}`}</h4>
+
 
             </section>
             <section>
@@ -23,7 +39,6 @@ const Home = () => {
             </section>
         </div>
     );
-    console.log(show);
 }
 
 export default Home
